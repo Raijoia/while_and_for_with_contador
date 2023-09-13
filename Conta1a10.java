dcc9c9dc9d4317353ff52db7f0e5d0b234619c17
Raí joia <megac154@gmail.com>
2023-09-12 21:40:07 -0300

    contador com while

---
+++ Conta1a10.java
@@ -0,0 +1,9 @@
+public class Conta1a10 {
+    public static void main(String[] args) {
+        int numero = 1;
+        while (numero <= 10) {
+            System.out.print(numero + " ");
+            numero = numero + 1;
+        }
+    }
+}
