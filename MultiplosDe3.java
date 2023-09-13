6580253e6949fe5ba18aa8acdf5ff11634722ee1
Raí <megac154@gmail.com>
2023-09-13 20:14:13 -0300

    Exercicio1: MultiplosDe3 ok

---
+++ MultiplosDe3.java
@@ -0,0 +1,8 @@
+public class MultiplosDe3 {
+  public static void main (String [] args) {
+    System.out.println("Multiplos de 3 entre 1 e 1000");
+    for(int cont = 3; cont <= 1000; cont = cont + 3){
+      System.out.print(cont + " ");
+    }
+  }
+}
